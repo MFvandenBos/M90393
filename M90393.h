2e852cb9e60bfe89706e1c9fc2f92d24b262af1c
MFvandenBos <35063713+MFvandenBos@users.noreply.github.com>
2019-01-23 11:38:08 +0100

    Update M90393.h

--- M90393.h
+++ M90393.h
@@ -6,28 +6,28 @@
 #ifndef M90393_h
 #define M90393_h
 #include "Energia.h"
-#define MOSI0 7
+#define MOSI0 7		//P1.5
 #define DELAY_TIME 1
-#define MISO0 6
-#define MISO1 8
-#define MISO2 9
-#define MISO3 10
-#define MISO4 23
-#define MISO5 24
-#define MISO6 25
-#define MISO7 26
-#define MISO8 27
-#define MISO9 28
-#define MISO10 29
-#define MISO11 30
-#define MISO12 40
-#define MISO13 39
-#define MISO14 38
-#define MISO15 37
-#define MISO16 36
-#define SCLK 2
-#define CS 5
-
+#define MISO0 6    	//P4.3
+#define MISO1 8		//P4.6
+#define MISO2 23	//P6.1
+#define MISO3 24
+#define MISO4 25
+#define MISO5 26
+#define MISO6 27
+#define MISO7 28
+#define MISO8 29
+#define MISO9 30
+#define MISO10 40
+#define MISO11 39
+#define MISO12 38
+#define MISO13 37
+#define MISO14 36
+#define MISO15 35
+#define MISO16 34
+#define SCLK 2		//P6.0
+#define CS 5		//4.1
+//SCL(1)=9 and SDA(1)=10, SDA(0) = 15, SCL(0) = 14 
 class M90393
 {
   public:
@@ -35,33 +35,64 @@ class M90393
 	// test functions
     int pintest();
     int pintest2();
+	void datamatrixrand();
+	void resetdatamatrix();
+	void resetdatamatrix(unsigned char zyxt, int ArraySize);
 	//command functions
     unsigned char cmdNOP();
     unsigned char cmdEX();
     unsigned char cmdSB(char zyxt);
     unsigned char cmdSWOC(char zyxt);
     unsigned char cmdSM(char zyxt);
+	void cmdRM(char zyxt);
+	unsigned char cmdRT();
+	unsigned char cmdWR(unsigned char c2, unsigned char c3, unsigned char c4);
+	unsigned short cmdRR(unsigned char c2, unsigned char c3, unsigned char c4);
 	// utility functions
     void writeMLX90393(unsigned char c);
 	unsigned char readMLX90393();
 	unsigned char sendCommand1(unsigned char c);
+	void readMLX90393_multiC(int ArraySize);
+	void readMLX90393_multiS(int ArraySize);
+	void transportMLX90393C(int ArraySize);
+	void transportMLX90393S(int ArraySize, int partsel);
+	void writeseqMLX90393(unsigned char c, unsigned char c2, unsigned char c3, unsigned char c4, int writebytes);
+	void sendCommandlong(unsigned char c, unsigned char c2, unsigned char c3, unsigned char c4, int ArraySize, int writebytes, int returnbytes);
+	void sendCommandlong(unsigned char c, unsigned char c2, unsigned char c3, unsigned char c4, int ArraySize, int writebytes, int returnbytes, unsigned char zyxt);
+	
+	// retrieval functions
+	float retrieve(int sensor, unsigned short mode);
+	unsigned char retrieveS(int sensor);
+	unsigned short boostRet(int sensor, unsigned short mode);
 	float getGainZ(int res, int gain);
 	float getGainXY(int res, int gain); 
-	
+	float getSenseZ();
+	float getSenseX();
+	float getSenseY();	
+	void readRes();
 	// example functions
 	void dot();
     void dash();
     
     int num_s;
+	unsigned short datamatrix[16][4];
+	unsigned char statusmatrix[16];
+	unsigned char charoutputmatrix[16];
+	unsigned short shortoutputmatrix[16];
+	float resultmatrix[16][4];
     int _selct;
     int sensorSelect;
    private:
   	int UsEL;
   	int _CS;
   	int _CLK;
-  	int _ERLED;
-	int _pin;
-
+	float sense_x;
+	float sense_y;
+	float sense_z;
+	unsigned short res_x;
+	unsigned short res_y;
+	unsigned short res_z;
+	bool knownRes;
   	char _MISO_P[16];
   	char _MOSI_P[16];
   	int _MISO[17];
