3236311c1d25512fc3337bdb7d1f3317e3a2bcba
MFvandenBos <35063713+MFvandenBos@users.noreply.github.com>
2019-03-18 16:43:22 +0100

    Update M90393.h

--- M90393.h
+++ M90393.h
@@ -6,9 +6,9 @@
 #ifndef M90393_h
 #define M90393_h
 #include "Energia.h"
-#define MOSI0 7		//P1.5
+#define MOSI0 6		
 #define DELAY_TIME 1
-#define MISO0 6    	//P4.3
+#define MISO0 7    	
 #define MISO1 8		//P4.6
 #define MISO2 23	//P6.1
 #define MISO3 24
@@ -32,6 +32,7 @@ class M90393
 {
   public:
     M90393(int sensors);
+	void mcinit();
 	// test functions
     int pintest();
     int pintest2();
@@ -47,7 +48,7 @@ class M90393
 	void cmdRM(char zyxt);
 	unsigned char cmdRT();
 	unsigned char cmdWR(unsigned char c2, unsigned char c3, unsigned char c4);
-	unsigned short cmdRR(unsigned char c2, unsigned char c3, unsigned char c4);
+	unsigned short cmdRR(unsigned char c2);
 	// utility functions
     void writeMLX90393(unsigned char c);
 	unsigned char readMLX90393();
@@ -59,10 +60,16 @@ class M90393
 	void writeseqMLX90393(unsigned char c, unsigned char c2, unsigned char c3, unsigned char c4, int writebytes);
 	void sendCommandlong(unsigned char c, unsigned char c2, unsigned char c3, unsigned char c4, int ArraySize, int writebytes, int returnbytes);
 	void sendCommandlong(unsigned char c, unsigned char c2, unsigned char c3, unsigned char c4, int ArraySize, int writebytes, int returnbytes, unsigned char zyxt);
+	unsigned char setFilter(unsigned char DIG_FILT);
+	unsigned char setOSR(unsigned char OSR, unsigned char OSR2);
+	unsigned char setADC(unsigned char OSR, unsigned char OSR2, unsigned char DIG_FILT, unsigned char RES_X, unsigned char RES_Y, unsigned char RES_Z);
+	
 	
 	// retrieval functions
 	float retrieve(int sensor, unsigned short mode);
 	unsigned char retrieveS(int sensor);
+	unsigned char getTcomp();
+	unsigned char getHallConF();
 	unsigned short boostRet(int sensor, unsigned short mode);
 	float getGainZ(int res, int gain);
 	float getGainXY(int res, int gain); 
@@ -70,19 +77,53 @@ class M90393
 	float getSenseX();
 	float getSenseY();	
 	void readRes();
+	void readTcomp();
+	void readHallConF();
+	int getResX();
+	// filter functions
+	void filter_init(float P0, float X_0, float Y_0, float Z_0, float R);
+	void filter(unsigned char zyxt);  
+	float retrieve_filter(int sensor, unsigned short mode);
+	float retrieve_raw(int sensor, unsigned short mode);
+	void Kalmanupdate(int count, int axis);
+	double qDet(double qold, double mq, double R_r);
+	void grab(unsigned char zyxt);
+	
+	//calibration functions
+	void temp_CAL();
+	
+	
 	// example functions
 	void dot();
     void dash();
     
+	// variables 
     int num_s;
 	unsigned short datamatrix[16][4];
 	unsigned char statusmatrix[16];
 	unsigned char charoutputmatrix[16];
 	unsigned short shortoutputmatrix[16];
-	float resultmatrix[16][4];
+	
+	
     int _selct;
     int sensorSelect;
    private:
+   // filter variables
+	float resultmatrix[16][4]; // grab takes variables, converts them into values and stores them in this matrix
+	float resultmatrixfilter[16][4]; // here filtered values are stored. filter updates them with the results from resultmatrix. 
+	float filtermatrix[16][4]; // stores Q, R, K, and P for the filter. 
+	float kmatrix[16][3]; // stores divergent K for each axis
+	float pmatrix[16][3]; // stores divergent P for each axis.  
+	unsigned short tREF_ACT[16]; //stores the reference temperatures for all sensors  
+	float offset_matrix[16][3]; // stores x, y and z offsets. It was chosen to use this matrix rather than the offset values on the MLX90393 chip because it gives better control. 
+	// Besides, the offset values on the chip are used by the temperature compensation algorithm on the chip. It is still not clear to me exactly how, but,
+	// It is clear that the offset values on the chip are used only if TC is turned on. If TC is turned off, there are also other problems to deal with. 
+	
+	
+	char Dig_filt_actual; //stores the actual value of digital filter
+	char OSR_actual; // stores the actual value of the OSR
+		
+	unsigned short TREF; 
   	int UsEL;
   	int _CS;
   	int _CLK;
@@ -92,7 +133,10 @@ class M90393
 	unsigned short res_x;
 	unsigned short res_y;
 	unsigned short res_z;
+	unsigned short tcomp;
+	unsigned short hallConF;
 	bool knownRes;
+	bool knowntcomp;
   	char _MISO_P[16];
   	char _MOSI_P[16];
   	int _MISO[17];
